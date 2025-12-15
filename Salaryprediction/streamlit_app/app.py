b688017688917faf061a42c0a8485412f519d0a9
VISWANTH-89 <viswanth752@gmail.com>
2025-12-15 09:30:05 +0530

    Implement salary prediction app using Streamlit

---
+++ Salaryprediction/streamlit_app/app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pickle
+
+st.title("💼 Salary Prediction App")
+
+model=pickle.load(open("model.pkl", "rb")) 
+st.subheader("🧑‍💻 Enter Details")
+
+years = st.number_input("Enter the Years of Experience", min_value=0.0, step=0.1)
+
+# Create DataFrame for prediction
+input_data = pd.DataFrame([[years_experience]], columns=["YearsExperience"])
+
+# Predict salary
+if st.button("Predict Salary"):
+    prediction = model.predict(input_data)[0]
+    st.success(f"💰 Predicted Salary: ₹ {prediction:,.2f}")
