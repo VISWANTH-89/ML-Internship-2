fe7cc6f1976f4ab159bcad67801c7f9406c86969
VISWANTH-89 <viswanth752@gmail.com>
2025-12-18 19:43:41 +0530

    Update app.py

--- Decisiontree_gamma/streamlit_app.py/app.py
+++ Decisiontree_gamma/streamlit_app.py/app.py
@@ -48,7 +48,7 @@ previous_score = st.number_input(
 if st.button("Predict"):
 
     # Automatic fail rules
-   if study_gamma <= 2 or score_gamma <= 200:
+   if study_hours <= 2 or previous_score <= 200:
     st.error("❌ Prediction: FAIL")
     st.warning("Reason: Study Hours ≤ 2 or Previous Exam Score ≤ 200")
 
