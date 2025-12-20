8ba88dcb83fc59d6d6098ba2fc62de2f47a849b5
VISWANTH-89 <viswanth752@gmail.com>
2025-12-20 10:36:53 +0530

    Create app.py

---
+++ instrument_frequency/streamlit_app.py/app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import numpy as np
+import librosa
+import matplotlib.pyplot as plt
+import pickle
+import time
+import tempfile
+
+# -------------------------------
+# App Config
+# -------------------------------
+st.set_page_config(page_title="Instrument Detection", layout="centered")
+st.title("🎵 Music Instrument Detection & Frequency Visualizer")
+
+# -------------------------------
+# Load PKL Model
+# -------------------------------
+with open("instrument_frequency_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+instrument_ranges = model["instruments"]
+
+# -------------------------------
+# Upload Audio
+# -------------------------------
+audio_file = st.file_uploader(
+    "Upload a song file (.wav or .mp3)",
+    type=["wav", "mp3"]
+)
+
+if audio_file is not None:
+
+    # Save uploaded file temporarily
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        tmp.write(audio_file.read())
+        audio_path = tmp.name
+
+    # Play audio
+    st.audio(audio_file, format="audio/mp3")
+
+    # Load audio
+    y, sr = librosa.load(audio_path, sr=None)
+
+    st.success("Audio loaded successfully!")
+
+    # -------------------------------
+    # FFT Parameters
+    # -------------------------------
+    frame_size_
