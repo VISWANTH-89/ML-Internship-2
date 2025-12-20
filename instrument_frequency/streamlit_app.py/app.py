d08cfc8bf3780dac537ce101332efe33a68b1edf
VISWANTH-89 <viswanth752@gmail.com>
2025-12-20 10:45:44 +0530

    Update app.py

--- instrument_frequency/streamlit_app.py/app.py
+++ instrument_frequency/streamlit_app.py/app.py
@@ -33,6 +33,28 @@ audio_file = st.file_uploader(
     "Upload a song file (.wav or .mp3)",
     type=["wav", "mp3"]
 )
+# FFT Parameters
+frame_size = 2048
+hop_length = 512
+
+fig, ax = plt.subplots()
+plot_area = st.pyplot(fig)
+
+for i in range(0, len(y) - frame_size, hop_length):
+
+    frame = y[i:i + frame_size]
+
+    fft = np.abs(np.fft.rfft(frame))
+    freqs = np.fft.rfftfreq(frame_size, 1 / sr)
+
+    ax.clear()
+    ax.plot(freqs, fft)
+    ax.set_xlim(0, 5000)
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_ylabel("Amplitude")
+    ax.set_title("Live Frequency Wave")
+
+    plot_area.pyplot(fig)
 
 if audio_file is not None:
 
